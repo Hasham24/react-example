866b3adc798c25c8fb81c94cc6a5efae3b737f1e
Ahmad Hasham <hasham@bitsol.tech>
2024-04-23 10:38:44 +0500

    left panel

---
+++ src/assets/images/index.ts
@@ -0,0 +1,3 @@
+export const Graphics = {
+  bgImage: require("./bg.png"),
+};
---
+++ src/components/left-component/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./styles.module.css";
+const LeftComponent: React.FC<any> = (props) => {
+  return <div className={styles.leftComponent}></div>;
+};
+export default LeftComponent;
